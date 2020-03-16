275b22ed246f024b2e0fe6b3088452a9dfe22192
cedric-winbush <cawinbushjr@gmail.com>
2020-03-16 15:07:46 -0500

    MVP -- Stretch on queries

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -36,20 +36,24 @@ router.get("/:id", (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
-  db('accounts')
-  .insert(req.body, "id")
-  .then(ids => {
-    db("accounts")
-    .where({ id: ids[0] }) //grabs the first id number that is returned in the ids array
-    .first() //this grabs the first element
-    .then(account => {
-      res.status(200).json({ data: account });
+router.post("/", (req, res) => {
+  db("accounts")
+    .insert(req.body, "id")
+    .then(ids => {
+      if (ids > 0) {
+        db("accounts")
+          .where({ id: ids[0] }) //grabs the first id number that is returned in the ids array
+          .first() //this grabs the first element
+          .then(account => {
+            res.status(200).json({ data: account });
+          });
+      } else {
+        res.status(404).json({ message: "Account Not Added" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "sorry, ran into an error" });
     });
-  })
-  .catch(error => {
-    res.status(500).json({ message: "sorry, ran into an error" });
-  });
 
   // db("accounts")
   //   .insert(req.body, "id")
@@ -59,8 +63,7 @@ router.post('/', (req, res) => {
   //   .catch(error => {
   //     res.status(500).json({ message: "sorry, ran into an error" });
   //   });
-
-})
+});
 
 router.put("/:id", (req, res) => {
   const changes = req.body;
